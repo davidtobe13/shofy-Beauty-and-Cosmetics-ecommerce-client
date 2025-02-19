2e02ed3a6700fd8b3a44182afa3dee081c4e46d0
davidtobe13 <davidtobe1999@gmail.com>
2025-02-19 05:39:53 +0100

    new

--- src/layout/footers/footer.jsx
+++ src/layout/footers/footer.jsx
@@ -89,7 +89,7 @@ const Footer = ({ style_2 = false, style_3 = false,primary_style=false }) => {
                           </span>
                         </div>
                         <div className="tp-footer-contact-content">
-                          <p><a href="https://www.google.com/maps/place/Sleepy+Hollow+Rd,+Gouverneur,+NY+13642,+USA/@44.3304966,-75.4552367,17z/data=!3m1!4b1!4m6!3m5!1s0x4cccddac8972c5eb:0x56286024afff537a!8m2!3d44.3304928!4d-75.453048!16s%2Fg%2F1tdsjdj4" target="_blank">Lagos Mainland. <br /> Lagos, Nigeria</a></p>
+                          <p><a href="https://www.google.com/maps/place/Lagos,+Nigeria/@6.5483694,3.1191445,11z/data=!3m1!4b1!4m6!3m5!1s0x103b8b2ae68280c1:0xdc9e87a367c3d9cb!8m2!3d6.5243793!4d3.3792057!16zL20vMGxuZnk?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D" target="_blank">Lagos Mainland. <br /> Lagos, Nigeria</a></p>
                         </div>
                       </div>
                     </div>
